3531a9c6883830d97c8677721e36900bfcc91a99
llyymm23 <jsy98bam1023@naver.com>
2024-04-24 02:28:35 +0900

    Feat : 채팅방 입장 멤버 옆 joined, 타이핑 중 멤버 표시,초대되지 않은 경우 다른 화면

--- src/page/chat/Chat.jsx
+++ src/page/chat/Chat.jsx
@@ -6,9 +6,6 @@ import { useCookies } from 'react-cookie';
 
 function Chat() {
     const [cookies] = useCookies(['Authorization']);
-
-    console.log("쿠키 정보 cookies : ", cookies);
-
     //룸id
     const { id } = useParams();
 
@@ -21,30 +18,59 @@ function Chat() {
     const [text, setText] = useState("")
     const [socket, setSocket] = useState(null);
 
-    //채팅방에 참여한 멤버 저장
+    // 채팅방 입장 상태(socket.on(joinroom)이 제대로 된 경우만 채팅방 보여줌)
+    const [isRoomJoined, setIsRoomJoined] = useState(false);
+
+    //채팅방에 참여한 멤버 저장(유저id) - 화면에서 채팅방 입장한 멤버 이름 옆에 joined라고 뜸
     const [joinedMembers, setJoinedMembers] = useState([]);
 
-    // //채팅 치는 중인 사람 보여줌
-    // const [isTyping, setIsTyping] = useState(false);
-    // const [typingDisplay, setTypingDisplay] = useState('');
+    //채팅 치는 중인 사람 보여줌
+    const [isTyping, setIsTyping] = useState(false);
+    const [typingDisplay, setTypingDisplay] = useState('');
+    const [typingTimeout, setTypingTimeout] = useState(null); // 타이핑 시간 재기
 
     //채팅창에 입력한 메시지 저장
     const handleMessage = (event) => {
         const newText = event.target.value;
         setText(newText);
+
+        // 타이핑 시작 또는 중단 
+        if (newText.length > 0 && !isTyping) {
+            socket?.emit('typing', { "roomId": id, "isTyping": true }); // 입력 중
+            setIsTyping(true);
+
+            // 타이핑 타이머 시작 (2초 후에 타이핑 상태 해제)
+            resetTypingTimeout();
+        } else if (newText.length === 0) {
+            resetTypingTimeout(); // 타이머 재설정
+
+        }
     }
+
+    //타이핑 하고 2초 후에 누가 타이핑 중이라는 메시지 없어짐
+    const resetTypingTimeout = () => {
+        if (typingTimeout) {
+            clearTimeout(typingTimeout);
+        }
+        setTypingTimeout(
+            setTimeout(() => {
+                setIsTyping(false);
+                socket?.emit('typing', { "roomId": id, "isTyping": false }); // 2초 후 타이핑 중단
+            }, 2000) // 2초 후 타이핑 해제
+        );
+    };
 
     //채팅창 입력한 메시지 서버로 전송
     const send = (text) => {
-        console.log("send 부분 내용 : ", text);
         socket?.emit('message', { "roomId": room.roomId, "chat": text });
+        setText(""); //서버로 메시지 전송 후 입력 창 초기화
+        setIsTyping(false); // 메시지 전송 후 타이핑 상태 초기화
     }
 
     //채팅방 정보/내용 조회
+    //조회 시 초대된 멤버만 가능
     const getChat = async () => {
         try {
-            console.log("roomId : ", id);
-
             const response = await axios.get(`http://localhost:3000/chat/room/${id}/content`, {
                 headers: {
                     Authorization: cookies.Authorization
@@ -60,10 +86,10 @@ function Chat() {
         } catch (error) {
             if (error.response) {
                 // 서버로부터 응답이 도착한 경우
-                alert("서버 오류: " + error.response.data.message);
+                console.log("서버 오류: " + error.response.data.message);
             } else if (error.request) {
                 // 요청이 서버에 도달하지 않은 경우
-                alert("서버에 요청할 수 없습니다.");
+                console.log("서버에 요청할 수 없습니다.");
             } else {
                 // 그 외의 경우
                 alert("오류가 발생했습니다: " + error.message);
@@ -73,6 +99,7 @@ function Chat() {
     }
 
     //채팅방 멤버 조회
+    //조회 시 멤버만 가능
     const getMember = async () => {
         try {
             const response = await axios.get(`http://localhost:3000/member/plan/${room.planId}`, {
@@ -91,10 +118,10 @@ function Chat() {
         } catch (error) {
             if (error.response) {
                 // 서버로부터 응답이 도착한 경우
-                alert("서버 오류: " + error.response.data.message);
+                console.log("서버 오류: " + error.response.data.message);
             } else if (error.request) {
                 // 요청이 서버에 도달하지 않은 경우
-                alert("서버에 요청할 수 없습니다.");
+                console.log("서버에 요청할 수 없습니다.");
             } else {
                 // 그 외의 경우
                 alert("오류가 발생했습니다: " + error.message);
@@ -105,8 +132,6 @@ function Chat() {
     }
 
     useEffect(() => {
-        console.log("소켓 보내는 쿠키 : ", cookies.Authorization);
-
         const newSocket = io("http://localhost:3000/events", {
             extraHeaders: {
                 Authorization: cookies.Authorization
@@ -132,12 +157,16 @@ function Chat() {
                 socket.emit('joinRoom', { roomId: id });
             };
 
+            //소켓이 연결되면 채팅방 참여 요청
             socket.on('connect', () => {
                 joinRoom();
                 console.log("Connected to Server");
-            }); // 소켓 연결되면 채팅방 참여 요청
+            });
 
-            socket.on('joinRoomSuccess', () => {
+            socket.on('joinRoomSuccess', (members) => {
+                console.log("룸 입장해 있는 members: ", members);
+                setIsRoomJoined(true); //프론트에서 멤버 검증
+                setJoinedMembers(members); // 입장 성공 시 멤버 리스트 업데이트
                 console.log('채팅방에 입장하였습니다.');
             });
 
@@ -148,6 +177,7 @@ function Chat() {
             });
 
             socket.on('joinRoomFailed', (error) => {
+                setIsRoomJoined(false);
                 console.error('채팅방 입장 실패 :', error.message);
                 alert('방에 참여할 수 없습니다.');
             });
@@ -160,7 +190,24 @@ function Chat() {
             };
         }
 
-    }, [socket])
+    }, [socket, id])
+
+    useEffect(() => {
+        if (socket) {
+            // 서버에서 타이핑 이벤트 수신
+            socket.on('typing', (data) => {
+                if (data.isTyping) {
+                    setTypingDisplay(`${data.nickname} is typing...`); // 타이핑 중인 사용자 표시
+                } else {
+                    setTypingDisplay(""); // 타이핑 상태 해제
+                }
+            });
+
+            return () => {
+                socket.off('typing'); // 컴포넌트 언마운트 시 이벤트 해제
+            };
+        }
+    }, [socket]);
 
     useEffect(() => {
         //서버에서 저장한 채팅 내용 가져옴
@@ -175,6 +222,16 @@ function Chat() {
         }
     }, [socket])
 
+    //채팅방 멤버 아닐 경우 해당 화면 보여줌
+    if (!isRoomJoined) {
+        return (
+            <div>
+                <h2>해당 채팅방에 입장할 수 없습니다.</h2>
+                <p>채팅방에 참여하려면 초대받은 멤버여야 합니다.</p>
+            </div>
+        );
+    }
+
     return (
         <div>
             {room && (
@@ -187,7 +244,7 @@ function Chat() {
             <ul>
                 {members.map((m) => (
                     <li key={m.memberId}>
-                        <strong>닉네임:</strong> {m.nickname} {joinedMembers.includes(m.memberId) ? "(joined)" : ""}
+                        <strong>닉네임:</strong> {m.nickname} {joinedMembers.includes(m.userId) ? "(joined)" : ""}
                         <br />
                         <strong>타입:</strong> {m.type} <br />
                     </li>
@@ -198,9 +255,13 @@ function Chat() {
                 <div>{message.name} : {message.chat}</div>
             )}
 
-
-            <input onChange={handleMessage} placeholder="Type your message..." value={text} />
-            <button onClick={() => send(text)}>Send</button>
+            <div>
+                {typingDisplay} {/* 타이핑 상태 표시 */}
+            </div>
+            <div>
+                <input onChange={handleMessage} placeholder="Type your message..." value={text} />
+                <button onClick={() => send(text)}>Send</button>
+            </div>
         </div>
     )
 }
