039f48fb79180c0536aa3ff1dd5e09a546f350d9
llyymm23 <jsy98bam1023@naver.com>
2024-04-23 20:38:41 +0900

    Feat : 채팅창 만들기

--- src/page/chat/Chat.jsx
+++ src/page/chat/Chat.jsx
@@ -1,36 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from 'react'
-import { Link, useParams } from "react-router-dom";
-import io, { Socket } from 'socket.io-client'
-import { Cookies } from 'react-cookie';
+import { useParams } from "react-router-dom";
+import io from 'socket.io-client'
+import { useCookies } from 'react-cookie';
 
-function App() {
-    const cookies = new Cookies();
+function Chat() {
+    const [cookies] = useCookies(['Authorization']);
+
+    console.log("쿠키 정보 cookies : ", cookies);
+
+    //룸id
     const { id } = useParams();
 
-    const [value, setValue] = useState("")
-    const [socket, setSocket] = useState(null);
-    const [messages, setMessages] = useState([]);
-    const [room, setRoom] = useState(null);
+    //현재 채팅방 정보, 멤버,그 전 채팅들 저장
+    const [room, setRoom] = useState([]);
     const [members, setMembers] = useState([]);
+    const [messages, setMessages] = useState([]);
 
-    const send = (value) => {
-        console.log("send 부분 value : ", value);
-        socket?.emit('message', value)
+    //입력받은 채팅,소켓 저장
+    const [text, setText] = useState("")
+    const [socket, setSocket] = useState(null);
+
+    //채팅방에 참여한 멤버 저장
+    const [joinedMembers, setJoinedMembers] = useState([]);
+
+    // //채팅 치는 중인 사람 보여줌
+    // const [isTyping, setIsTyping] = useState(false);
+    // const [typingDisplay, setTypingDisplay] = useState('');
+
+    //채팅창에 입력한 메시지 저장
+    const handleMessage = (event) => {
+        const newText = event.target.value;
+        setText(newText);
+    }
+
+    //채팅창 입력한 메시지 서버로 전송
+    const send = (text) => {
+        console.log("send 부분 내용 : ", text);
+        socket?.emit('message', { "roomId": room.roomId, "chat": text });
     }
 
     //채팅방 정보/내용 조회
     const getChat = async () => {
         try {
-            const token = cookies.get('access_token');
-
             console.log("roomId : ", id);
 
             const response = await axios.get(`http://localhost:3000/chat/room/${id}/content`, {
                 headers: {
-                    Authorization: `Bearer ${token}`
-                },
-                withCredentials: true
+                    Authorization: cookies.Authorization
+                }, withCredentials: true
             });
 
             const chat = response.data.chat;
@@ -57,20 +75,18 @@ function App() {
     //채팅방 멤버 조회
     const getMember = async () => {
         try {
-            const token = cookies.get('access_token');
-
-            const response = await axios.get(`http://localhost:3000/member/plan/${room.planId}}`, {
+            const response = await axios.get(`http://localhost:3000/member/plan/${room.planId}`, {
                 headers: {
-                    Authorization: `Bearer ${token}`
-                },
-                withCredentials: true
+                    Authorization: cookies.Authorization
+                }, withCredentials: true
+
             });
 
-            const members = response.data.members;
+            const member = response.data.members;
 
-            console.log("채팅방 멤버 조회 : ", members);
+            console.log("채팅방 멤버 조회 : ", member);
 
-            setMembers(members);
+            setMembers(member);
 
         } catch (error) {
             if (error.response) {
@@ -89,65 +105,66 @@ function App() {
     }
 
     useEffect(() => {
-        const token = cookies.get('access_token');
+        console.log("소켓 보내는 쿠키 : ", cookies.Authorization);
 
-        const newSocket = io("http://localhost:3001/events", {
+        const newSocket = io("http://localhost:3000/events", {
             extraHeaders: {
-                Authorization: `Bearer ${token}`
-            }
+                Authorization: cookies.Authorization
+            }, withCredentials: true
         });
 
-        setSocket(newSocket)
-            }, [setSocket])
-
-    useEffect(() => {
+        setSocket(newSocket);
         getChat();
-
-    }, [])
+    }, [setSocket])
 
     useEffect(() => {
         // room 정보가 설정되었을 때 멤버 조회
         if (room) {
             getMember();
         }
-
-        // 'addMember' 이벤트를 리스닝하고, 새로운 멤버를 추가
-        socket.on('addMember', (newMember) => {
-            setMembers((prevMembers) => [...prevMembers, newMember]);
-        });
-
-        return () => {
-            // 컴포넌트가 언마운트될 때 소켓 이벤트를 해제
-            socket.off('addMember');
-        };
-
     }, [room]); // room 상태가 변경될 때 getMember 실행
 
     useEffect(() => {
-        socket?.on('connected', () => {
-            console.log('Connected to server');
-        });
+        if (socket) {
+            //채팅방 참여하기(현재 유저가 채팅방 들어옴)
+            //채팅방 링크로 멤버가 들어오면 그 멤버가 플랜에 초대된 멤버인지 서버 쪽에서 검증 후 맞으면 joinRoom
+            const joinRoom = () => {
+                socket.emit('joinRoom', { roomId: id });
+            };
 
-        socket?.on('addMember', (member) => {
-            console.log("member : ", member);
-            console.log(`${member.name}이 초대되었습니다.`);
-        })
+            socket.on('connect', () => {
+                joinRoom();
+                console.log("Connected to Server");
+            }); // 소켓 연결되면 채팅방 참여 요청
 
-        socket?.on('newMessage', (message) => {
-            console.log("newMessage : ", message);
-        })
+            socket.on('joinRoomSuccess', () => {
+                console.log('채팅방에 입장하였습니다.');
+            });
 
-        return () => {
-            socket?.off('connected');
-            socket?.off('newRoom');
-            socket?.off('addMember');
-            socket?.off('newMessage');
+            socket.on('memberJoined', (payload) => {
+                console.log(`새 멤버 참여: ${payload.userId}`);
+                // 새로운 멤버 참여 - 참여한 멤버 저장
+                setJoinedMembers((prev) => [...prev, payload.userId]);
+            });
+
+            socket.on('joinRoomFailed', (error) => {
+                console.error('채팅방 입장 실패 :', error.message);
+                alert('방에 참여할 수 없습니다.');
+            });
+
+            return () => {
+                socket.off('connect', joinRoom); // 컴포넌트 언마운트 시 이벤트 해제
+                socket.off('memberJoined'); // 추가된 이벤트 해제
+                socket.off('joinRoomFailed');
+                socket.off('joinRoomSuccess'); // 연결 해제 시 이벤트 해제
+            };
         }
+
     }, [socket])
 
     useEffect(() => {
+        //서버에서 저장한 채팅 내용 가져옴
         const messageListener = (message) => {
-            console.log("messageListener에서 message가 뭔지 : ", message);
             setMessages(prevMessages => [...prevMessages, message]);
         }
 
@@ -159,21 +176,33 @@ function App() {
     }, [socket])
 
     return (
-        <>
-            <input onChange={(e) => setValue(e.target.value)} placeholder="Type your message..." value={value} />
-            <button onClick={() => send(value)}>Send</button>
-            {messages.map((message, index) =>
-                <div key={index}>{message}</div>
-            )}
-
+        <div>
             {room && (
                 <div>
-                    <h2>현재 Room 이름</h2>
-                    <p>{room.name}</p>
+                    <h2>현재 Room 이름 : {room.name}</h2>
                 </div>
             )}
-        </>
+
+            <h3>채팅방 멤버</h3>
+            <ul>
+                {members.map((m) => (
+                    <li key={m.memberId}>
+                        <strong>닉네임:</strong> {m.nickname} {joinedMembers.includes(m.memberId) ? "(joined)" : ""}
+                        <br />
+                        <strong>타입:</strong> {m.type} <br />
+                    </li>
+                ))}
+            </ul>
+
+            {messages.map((message) =>
+                <div>{message.name} : {message.chat}</div>
+            )}
+
+
+            <input onChange={handleMessage} placeholder="Type your message..." value={text} />
+            <button onClick={() => send(text)}>Send</button>
+        </div>
     )
 }
 
-export default App
+export default Chat;
